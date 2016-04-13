d0271d77e7ee3f9f3caa38dc7c1d0a533de52f6b
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-04-12 20:50:07 -0500

    Make it easier to configure ports with Hiera

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,11 @@
 # Internal: configuration values for Cassandra
 #
-class cassandra::config {
+class cassandra::config (
+  $storage_port = 17000
+  $ssl_storage_port = 17001
+  $rpc_port = 19160
+  $native_port = 19042
+) {
   require boxen::config
 
   $configdir  = "${boxen::config::configdir}/cassandra"
@@ -8,10 +13,4 @@ class cassandra::config {
   $executable = "${boxen::config::home}/homebrew/bin/cassandra"
   $logdir     = "${boxen::config::logdir}/cassandra"
   $cluster    = 'github-dev'
-
-
-  $storage_port = 17000
-  $ssl_storage_port = 17001
-  $rpc_port = 19160
-  $native_port = 19042
 }
